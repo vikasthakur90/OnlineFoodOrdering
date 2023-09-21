5240e5d85bf81084e9cca654084fd904257de016
Vikas <vikasthakur.90900@gmail.com>
2023-09-21 14:49:37 +0530

    ep6

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,14 +1,31 @@
 import ResCard from "./ResCard";
-
+import { restaurants } from "../utils/mockData";
+import { useState,useEffect } from "react";
 
 const Body =()=>{
+    const [listOfRestaurants,setlistOfRestaurants] = useState(restaurants);
+    useEffect(()=>{
+        fetchData();
+    },[]);
+    const fetchData = async ()=>{
+      //  const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.7195687&lng=75.8577258&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+       // const json = await data.json();
+        //console.log(json);
+    }
+   if(listOfRestaurants === 0){
+    return <Shimmer />
+   }
     return(
         <div className="body">
         <div className="search">
-            <h4>search</h4>
+           <button className="btn-filter" onClick={()=>{
+            const filteredData = listOfRestaurants.filter(res=>res.info.avgRating>4);
+            setlistOfRestaurants(filteredData);
+           }}>Top rated restaurants</button>
+
         </div>
         <div className="res-container">
-         {restaurants.map(res => <ResCard key ={res.info.id} resData = {res}/>)}
+         {listOfRestaurants.map(res => <ResCard key ={res.info.id} resData = {res}/>)}
         </div>
 
         </div>
