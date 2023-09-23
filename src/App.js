f9ea56f8bdb4a26683e7deb16bc0809e6debf446
Vikas <vikasthakur.90900@gmail.com>
2023-09-23 14:11:54 +0530

    ep7part3

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./component/header";
 import Body from "./component/Body";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
 import About from "./component/About";
 import Error from "./component/Error";
+import Contact from "./component/Contact";
 
 
 const AppLayout = () =>{
@@ -12,7 +13,7 @@ const AppLayout = () =>{
     return(
         <div className="app">
             <Header/>
-            <Body />
+            <Outlet />
         </div>
     )
 }
@@ -20,12 +21,23 @@ const appRouter = createBrowserRouter([
     {
         path:"/",
         element:<AppLayout />,
+        children:[
+            {
+                path:"/",
+                element:<Body />
+            },
+            {
+                path:"/about",
+                element:<About />
+            },
+            {
+                path:"/contact",
+                element:<Contact />
+            }
+        ],
         errorElement: <Error />
     },
-    {
-        path:"/about",
-        element:<About />
-    }
+   
 ])
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={appRouter} />);
--- src/component/Body.js
+++ src/component/Body.js
@@ -3,19 +3,19 @@ import { restaurants } from "../utils/mockData";
 import { useState,useEffect } from "react";
 
 const Body =()=>{
-    const [listOfRestaurants,setlistOfRestaurants] = useState(restaurants);
+    const [listOfRestaurants,setlistOfRestaurants] = useState([]);
     const [filteredRestaurants,setfilteredRestaurants] = useState([]);
     const [searchText,setsearchText] = useState("");
     useEffect(()=>{
         fetchData();
     },[]);
     const fetchData = async ()=>{
-        //const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.7195687&lng=75.8577258&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-        //const json = await data.json();
-        //console.log(json);
-      //  setlistOfRestaurants(json?.data?.cards[2]?.data?.cards)
-      setlistOfRestaurants(restaurants);
-      setfilteredRestaurants(restaurants);
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.7195687&lng=75.8577258&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        console.log(json);
+        setlistOfRestaurants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+      //setlistOfRestaurants(restaurants);
+        setfilteredRestaurants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
     }
     //Conditional Rendering
   
@@ -23,11 +23,11 @@ const Body =()=>{
         <div className="body">
         <div className="filter">
             <input className="search-text" value={searchText} onChange={(e)=>{
-                setsearchText(e.target.value)
+                setsearchText(e.target.value);
+                
             }}></input>
             <button className="search-btn" onClick={()=>{
                 const filter1 = listOfRestaurants.filter(res=>res.info.name.toLowerCase().includes(searchText.toLowerCase()));
-             
                 setfilteredRestaurants(filter1);
             }}>Search</button>
 
